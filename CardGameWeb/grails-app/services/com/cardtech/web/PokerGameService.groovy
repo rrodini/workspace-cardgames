8b15ab7f7749866f804abee01a59ae7e441c8746
Robert Rodini <rrodini@hotmail.com>
2015-06-19 20:32:40 -0400

    Ability to play a single or multiple games.

--- CardGameWeb/grails-app/services/com/cardtech/web/PokerGameService.groovy
+++ CardGameWeb/grails-app/services/com/cardtech/web/PokerGameService.groovy
@@ -13,15 +13,84 @@ class PokerGameService {
 	
 	static scope = 'prototype'
 	static transactional = true // true => each method below constitutes an atomic transaction.
+	def sessionFactory	// this is to get the hibernate session
 
-    def playGame(List<Player> playerObjs) { // playerObjs is a list of domain objects
+    def playGames(List<Player> playerObjs, int numOfGames) { // playerObjs is a list of domain objects
 		// 
+		def result
+		Date firstGame = new Date();  // this is the timestamp for the first game
+		// To get all games ever played set firstGame via the SELECT below
+		//def firstGame = com.cardtech.web.PokerGame.executeQuery("select min(dateCreated) from PokerGame")
+		//firstGame = firstGame[0]
+		for (int i=0; i < numOfGames; i++) {
+			result = playSingleGame(playerObjs)
+		}
+		if (numOfGames == 1) {
+			return result
+		} else {
+			// return a summary of the games played
+			// get SQL timestamp of last game (also a Date) from the database
+			def lastGame = com.cardtech.web.PokerGame.executeQuery("select max(dateCreated) from PokerGame")
+			// above returns an array list of timestamps.
+			lastGame = lastGame[0]
+			def criteria = com.cardtech.web.GameParticipation.createCriteria()
+			// get GameParticipation outcomes between two dates
+			def outcomeList = criteria.list{
+					pokerGame {
+						between ('dateCreated', firstGame, lastGame)
+					}
+				}
+			def playerWins = [:]
+			outcomeList.each { game -> 
+println "GP: ${game.id}, ${game.player.id}, ${game.outcome} "
+				int win = (game.outcome == 'W')? 1 : 0
+
+				playerWins.put(game.player.id, playerWins.get(game.player.id,0)+win) 
+			}
+			// find the highest winning rank in poker games played between timestamps for given player
+		    def playerHighestWinningRank = [:]
+		    playerObjs.each { playerObj ->
+println "Determine highest rank for playerId: ${playerObj.id}"
+// Hibernate query below doesn't work.  Don't know why.
+//				def query = hibSession.createQuery(
+//"select max(PG.winningRank.id) from com.cardtech.web.PokerGame as PG inner join PG.gamesParticipants as GP where (PG.dateCreated between :start and :end) and (GP.player.id = :playerId and GP.outcome = 'W')")
+// 				query.setDate('start', firstGame)
+//				query.setDate('end',   lastGame)
+//				query.setLong('playerId', playerObj.id)
+//				def rankList = query.list()
+				def rankList = com.cardtech.web.PokerGame.executeQuery(
+					"select max(PG.winningRank.id) from com.cardtech.web.PokerGame as PG inner join PG.gamesParticipants as GP where (PG.dateCreated between :start and :end) and (GP.player.id = :playerId and GP.outcome = 'W')",
+					[start: firstGame, end: lastGame, playerId: playerObj.id ])
+				def rankId = null
+				// query returns one record containing a PokerRank id.
+				if (rankList?.size() > 0) {
+					rankId = rankList?.get(0)
+					playerHighestWinningRank.putAt(playerObj.id, rankId)
+					println "Highest rank: playerId: ${playerObj.id}, ${rankId}"
+				}
+		    }
+			// return a map
+			return [numOfGames: numOfGames, playerObjs: playerObjs, playerWins: playerWins, 
+				    playerHighestWinningRank: playerHighestWinningRank]
+// Below is sample of results map
+//		def results = [numOfGames: numOfGames,
+//		   playerObjs: playerObjs, 
+//           playerWins: [1L: 2, 3L:3, 4L:1], 
+//		   playerHighestWinningRank: [1L: 6L, 3L:3L, 4L:2L] ]
+//
+		}
+    }
+	
+	
+	
+	// playSingleGame returns a map used by the showGame view
+	def playSingleGame(List<Player> playerObjs) {
 		// Convert domain objects to card.cardtech.game.Player objects
 		com.cardtech.game.PokerPlayer [] players = new PokerPlayer[playerObjs.size()]
 		playerObjs.eachWithIndex { p,i ->
 			players[i] = new com.cardtech.game.PokerPlayer(p.firstName)
 		}
-		// create new PokerGame(players)
+		// create new PokerGame with given players
 		com.cardtech.game.PokerGame game = new com.cardtech.game.PokerGame(players)
 		// shuffle the cards before playing a game
 		game.initialize(true)
@@ -55,12 +124,12 @@ class PokerGameService {
 			partObj.pokerGame = gameObj
 			log.debug "player id: ${p.id}"
 			log.debug "game   id: ${gameObj.id}"
-			int noWinners = winners.size() 
+			int noWinners = winners.size()
 			if (winnerNames.contains(p.firstName)) {
 				partObj.outcome = (noWinners == 1)? "W" : "T"
- 			} else {
-			 	partObj.outcome = "L"
- 			}
+			 } else {
+				 partObj.outcome = "L"
+			 }
 			if (partObj.validate()) {
 				partObj.save(flush:true)
 			} else {
@@ -89,10 +158,9 @@ class PokerGameService {
 			}
 
 		}
-		// return a map to the controller who called
+		// return a map
 		[players: playerObjs.collect{it.firstName}, hands: hands, winners: winners, winningRank: winningRank]
-		
-    }
+	}
 	
 	// use the names of the cards to find to domain objects
 	private def assignPokerHandObj(PokerHand pokerHandObj, com.cardtech.game.PokerHand hand) {
