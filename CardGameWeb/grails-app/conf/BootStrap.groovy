d983d6c84d46d6b9f8639a76f9654b5d4c8633f6
Robert Rodini <rrodini@hotmail.com>
2015-06-19 20:19:34 -0400

    Added comments such that the ACE_HEART is not created.  Good for Selenium testing.

--- CardGameWeb/grails-app/conf/BootStrap.groovy
+++ CardGameWeb/grails-app/conf/BootStrap.groovy
@@ -7,35 +7,45 @@ class BootStrap {
     def init = { servletContext ->
 		for (Card c: Card.values()) {
 			String cardName = c.toString()
-			System.out.printf("Creating playing card: %s", cardName)
+			// skip the Ace of Hearts by uncommenting the next three lines.
+//			if (cardName.equalsIgnoreCase("ACE_HEART")) {
+//				continue;
+//			}
 			PlayingCard pc = new PlayingCard()
 			pc.name = cardName
-			if (pc.validate()) {
-				pc.save()
-			} else {
-				println pc.getErrors()
+			if (!PlayingCard.findByName(cardName)) {
+				if (pc.validate()) {
+					System.out.printf("Creating playing card: %s", cardName)
+					pc.save()
+				} else {
+					println pc.getErrors()
+				}
 			}
 		}
 		for (PokerRank r: PokerRank.values()) {
 			String rankName = r.toString()
-			System.out.printf("Creating poker rank: %s", rankName)
 			com.cardtech.web.PokerRank pr = new com.cardtech.web.PokerRank()
 			pr.name = rankName
-			if (pr.validate()) {
-				pr.save()
-			} else {
-				println pr.getErrors()
+			if (!com.cardtech.web.PokerRank.findByName(rankName)) {
+				if (pr.validate()) {
+					System.out.printf("Creating poker rank: %s", rankName)
+					pr.save()
+				} else {
+					println pr.getErrors()
+				}
 			}
 		}
 		
 		['Bob','Ed','Fred','Dave'].each { name ->
-			System.out.println("Creating player: $name")
 			com.cardtech.web.Player player = new com.cardtech.web.Player(firstName: name)
-			if (player.validate()) {
-				player.save()
-			} else {
-				println player.getErrors()
-			} 
+			if (!com.cardtech.web.Player.findByFirstName(player.firstName)) {
+				if (player.validate()) {
+					System.out.printf("Creating player: %s", player.firstName)
+					player.save()
+				} else {
+					println player.getErrors()
+				}
+			}
 		}
 		
     }
